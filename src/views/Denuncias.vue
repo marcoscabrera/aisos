cffd233a53569fcaceaa8188c68e94b4a45bdf3e
marcos cabrera <dev.marcoscabrera@gmail.com>
2020-10-07 20:55:32 -0500

    en denuncias denuevo

--- src/views/Denuncias.vue
+++ src/views/Denuncias.vue
@@ -1,11 +1,15 @@
 <template>
   <v-container name="contenedordenuncias">
-    <h2>en denuncias</h2>
+    <h2>Reporte Inicial</h2>
+    <reporteInicialEtapa></reporteInicialEtapa>
   </v-container>
 </template>
 <script>
+import reporteInicialEtapa from '@/components/etapas/reporteInicialEtapa.vue';
 export default {
   name: "Denuncias",
+
+  components : {  reporteInicialEtapa } 
 
   data() {
     return {};
