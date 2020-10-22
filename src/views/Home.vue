808d6cc695512b68d017332dd2eae3451d3b3c46
marcos cabrera <dev.marcoscabrera@gmail.com>
2020-10-22 09:31:47 -0500

    se cambian titulos en home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,31 +10,33 @@
     <v-row>
       <v-col></v-col>
       <v-col>
-            <img
-      alt="Vue logo"
-      src="https://www.aldeasinfantiles.org.mx/getmedia/51757cf7-a274-4bdc-b552-4304db77c698/logo-aldeas-mx"
-    />
+        <img
+          alt="Vue logo"
+          src="https://www.aldeasinfantiles.org.mx/getmedia/51757cf7-a274-4bdc-b552-4304db77c698/logo-aldeas-mx"
+        />
       </v-col>
       <v-col></v-col>
     </v-row>
 
     <v-row>
-    <v-col></v-col>
-    <v-col>Alcances</v-col>
-    <v-col></v-col>
+      <v-col></v-col>
+      <v-col>Alcances</v-col>
+      <v-col></v-col>
     </v-row>
 
     <v-row>
-    <v-col></v-col>
-    <v-col>
-      <ul>
-        <li>Distribucion de acceso en barra lateral</li>
-        <li>Presentación de vista de Dashboard y navegación</li>
-        <li>Presentación de vista de Valoracion Inicial </li>
-        <li>Presentacion de vista de configuración (catalogos)</li>
-      </ul>
-    </v-col>
-    <v-col></v-col>
+      <v-col></v-col>
+      <v-col>
+        <ul>
+          <li>Se realizan correciones en valoracion integral para VoBo</li>
+          <li>Se realizan correciones en seguimiento para VoBo</li>
+          <li>Se realizan correciones en cierre para VoBo</li>
+
+          <li>Presentación de vista Denuncia legal</li>
+          <li>Presentación de vista de Investigacion interna</li>
+        </ul>
+      </v-col>
+      <v-col></v-col>
     </v-row>
   </v-container>
 </template>
