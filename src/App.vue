71ac8dc07df2e97187c29c92731c1efd6835aa11
marcos cabrera <dev.marcoscabrera@gmail.com>
2021-04-12 10:18:43 -0500

    se corrige el titulo de la plataforma para que se vea bien en todos las vistas responsivas

--- src/App.vue
+++ src/App.vue
@@ -97,9 +97,9 @@
      
       </v-img></a>
       <v-spacer></v-spacer>
-      <v-app-bar-title :class="cssResponsiveTexto">..Plataforma interna de protección infantil SOS México</v-app-bar-title>
+      <v-app-bar-title width="100%" :class="cssResponsiveTexto">Plataforma interna de protección infantil SOS México</v-app-bar-title>
     
-
+      
      <ComponenteCardUsuarioLogueado>
 
      </ComponenteCardUsuarioLogueado>
