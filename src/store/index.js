3c3b1f225b0e27cc896df5a9b96610980e9e3a08
marcos cabrera <dev.marcoscabrera@gmail.com>
2021-07-26 16:25:21 -0500

    cambio hacia produccuion

--- src/store/index.js
+++ src/store/index.js
@@ -30,18 +30,18 @@ const store =  new Vuex.Store({
 
 
 ///////////////////////////////////////
-// API PRODUCCION ** servidor de produccion
+// API PRODUCCION ** servidor de produccion//
 ///////////////////////////////////////  
 
 
 
-//urlServidor : 'https://apiproteccioninfantil3.azurewebsites.net',
+urlServidor : 'https://apiproteccioninfantil3.azurewebsites.net',
    
 
 ///////////////////////////////////////
 // API DEMO
 /////////////////////////////////////// 
-urlServidor : 'https://apidemo2.azurewebsites.net',
+//urlServidor : 'https://apidemo2.azurewebsites.net',
 
 
 
